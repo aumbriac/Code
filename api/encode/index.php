c5a4644b9e168d78a0cfe0d3f0c62e4e84cd2265
Anthony Alexander <anthonyumbriac@gmail.com>
2020-04-22 16:27:06 -0700

    Shift Cipher API
    This API provides users with the ability to encode messages with a shift cipher.

---
+++ api/encode/index.php
@@ -0,0 +1,42 @@
+<?php
+
+$_SERVER['SERVER_PORT'] = 23456;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+function str_rot($message, $shift = 13) {
+    static $letters = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
+    $shift = (int)$shift % 26;
+    if (!$shift) return $message;
+    if ($shift < 0) $shift += 26;
+    if ($shift == 13) return str_rot13($message);
+    $rep = substr($letters, $shift * 2) . substr($letters, 0, $shift * 2);
+    return strtr($message, $letters, $rep);
+}
+
+    if ($_SERVER["CONTENT_TYPE"] != 'application/json') {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error');
+        exit();
+    }
+
+    $content = trim(file_get_contents('php://input'));
+    $decoded = json_decode($content, true);
+    $decodedShift = $decoded['Shift'];
+    $decodedMessage = $decoded['Message'];
+    if ($decodedShift && $decodedMessage && is_int($decodedShift) && is_string($decodedMessage)){
+        $data = str_rot($decodedMessage, $decodedShift);
+        $newFile = fopen("code".uniqid().".txt", "w");
+        $textContent = json_encode($data);
+        fwrite($newFile, str_replace('"', '', $textContent));
+    } else {
+        http_response_code(500);
+        exit();
+    }
+
+    header('Content-Type: application/json');
+    $array = array('EncodedMessage' => $data);
+    echo json_encode($array);
+
+} else {
+    exit("API Running on port " . $_SERVER['SERVER_PORT']);
+}
